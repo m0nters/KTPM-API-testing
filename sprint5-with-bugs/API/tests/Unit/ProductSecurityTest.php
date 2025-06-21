94f7b067d4d66cb6b0d3142806d4901ec5cf64ca
HuyPhanDev-2025 <ptqhuy1606@gmail.com>
2025-06-22 00:32:28 +0700

    change to push

--- sprint5-with-bugs/API/tests/Unit/ProductSecurityTest.php
+++ sprint5-with-bugs/API/tests/Unit/ProductSecurityTest.php
@@ -1,5 +1,5 @@
 <?php
-
+# abc
 namespace Tests\Unit;
 
 use Tests\TestCase;
@@ -22,17 +22,17 @@ class ProductSecurityTest extends TestCase
 
         // Make request as non-admin user
         $customer = User::factory()->create(['role' => 'customer']);
-        
+
         $response = $this->actingAs($customer, 'api')
             ->getJson('/products');
 
         $response->assertStatus(ResponseAlias::HTTP_OK);
-        
+
         // Check that stock information is not exposed to customers
         $responseData = $response->json();
-        
+
         foreach ($responseData['data'] as $productData) {
-            $this->assertArrayNotHasKey('stock', $productData, 
+            $this->assertArrayNotHasKey('stock', $productData,
                 'Stock information should not be revealed to customers');
             $this->assertArrayNotHasKey('is_location_offer', $productData,
                 'Location offer information should not be revealed to customers');
@@ -48,12 +48,12 @@ class ProductSecurityTest extends TestCase
 
         // Make request as admin user
         $admin = User::factory()->create(['role' => 'admin']);
-        
+
         $response = $this->actingAs($admin, 'api')
             ->getJson('/products');
 
         $response->assertStatus(ResponseAlias::HTTP_OK);
-        
+
         // Admin should be able to see stock information
         $responseData = $response->json();
         $this->assertArrayHasKey('stock', $responseData['data'][0] ?? [],
